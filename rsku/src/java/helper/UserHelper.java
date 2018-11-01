3db8605a3ce3d6c42cdcc27c390d51b8e7cf62ce
puspaningtyas <basisb33@BASIS-B33.basisdata.ti.usd>
2018-11-01 16:46:40 +0700

    koneksi ke server 202

---
+++ rsku/src/java/helper/UserHelper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+import pojos.User;
+
+/**
+ *
+ * @author basisb33
+ */
+public class UserHelper {
+
+    public UserHelper() {
+    }
+    
+    public User getUser(String email){
+        User user = new User();
+        return user;
+    }
+}
