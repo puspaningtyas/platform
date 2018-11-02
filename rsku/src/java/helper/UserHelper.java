d275397fd2243e1508588fae3b5b9b6a6a3c4bfd
puspaningtyas <basisa25@BASIS-A25.basisdata.ti.usd>
2018-11-02 10:07:51 +0700

    tambah login

--- rsku/src/java/helper/UserHelper.java
+++ rsku/src/java/helper/UserHelper.java
@@ -5,7 +5,12 @@
  */
 package helper;
 
+import java.util.Collections;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import pojos.User;
+import util.RsKuHibernateUtil;
 
 /**
  *
@@ -15,9 +20,38 @@ public class UserHelper {
 
     public UserHelper() {
     }
-    
-    public User getUser(String email){
-        User user = new User();
-        return user;
+
+    public List<User> getAllUser() {
+        List<User> result = null;
+        Session session = RsKuHibernateUtil.getSessionFactory().openSession();
+        String query = "from User u";
+        Query q = session.createQuery(query);
+        result = q.list();
+        session.close();
+        return result;
+    }
+
+    public User getUser(String username, String password) {
+        List<User> list = this.getAllUser();
+        Collections.sort(list);
+        User user = new User(username, password);
+        int index = Collections.binarySearch(list, user);
+        
+        if (index < 0) {
+            // user tiada
+            return null;
+        } else{
+            // user ada
+            User result = list.get(index);
+//            System.out.println("Pencarian = "+result.getEmail()+","+result.getPassword());
+            if (password.equals(result.getPassword())) {
+                // pass bener
+                System.out.println("Password = "+password);
+                return user;
+            } else {
+                // pass salah
+                return null;
+            }
+        } 
     }
 }
